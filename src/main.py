1567ee75d7b3101bcb38100f0d5c599ec51d549a
Bernardo Silva Sant Ana de Oliveira <bernardo.silva@estudante.ufjf.br>
2026-04-18 23:53:52 -0300

    atualiza e finaliza main

--- src/main.py
+++ src/main.py
@@ -1,19 +1,55 @@
-from data_loader import carregar_dados
+from data_loader import carregar_dados_jogador
+from cleaning import limpar_dataframe
 from tendencia_recente import tendencia_recente
 from tendencia_vs_time import tendencia_vs_time
 
+
 def main():
-    df = carregar_dados()
+    print("\n=== ANALISADOR DE PROPS NBA ===\n")
 
-    print("----ANALISADOR DE PROPS NBA----")
-    coluna = input("Estatística você quer analisar? (ex: PONTOS, REBOTES): ")
-    limite = float(input("Limite da aposta (ex: 25): "))
-    adversario = input("Adversário do jogador (ex: BOS): ")
-    ultimos_jogos = int(input("Número de jogos recentes para análise (ex: 5): "))
+    jogador = input("Nome do jogador: ")
 
-    print("\nRESULTADOS DA ANÁLISE:\n")
-    tendencia_recente(df, coluna, limite, ultimos_jogos)
-    tendencia_vs_time(df, adversario, coluna, limite)
+    estatistica = input(
+        "Estatística (PONTOS / ASSISTENCIAS / REBOTES / C3_CONVERTIDOS / LL_CONVERTIDOS): "
+    ).upper()
+
+    linha = float(input("Linha da aposta: "))
+
+    adversario = input("Adversário (ex: BOS, CHA, LAL): ").upper()
+
+    ultimos_jogos = int(input("Últimos quantos jogos analisar? "))
+
+
+    df = carregar_dados_jogador(jogador)
+
+    if df is None:
+
+        return
+
+
+    df = limpar_dataframe(df)
+
+
+    print("\n📊 Tendência recente:\n")
+
+    tendencia_recente(
+        df,
+        estatistica,
+        linha,
+        ultimos_jogos
+    )
+
+
+    print("\n🏀 Tendência vs adversário:\n")
+
+    tendencia_vs_time(
+        df,
+        adversario,
+        estatistica,
+        linha
+    )
+
 
 if __name__ == "__main__":
+
     main()
