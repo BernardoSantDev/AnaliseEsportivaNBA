caf38846621d225746e9bded4f78907cbc3221a6
Bernardo Silva Sant Ana de Oliveira <bernardo.silva@estudante.ufjf.br>
2026-04-19 00:02:19 -0300

    mostra os ultimos jogos

--- src/tendencia_recente.py
+++ src/tendencia_recente.py
@@ -1,5 +1,6 @@
 def tendencia_recente(df, estatistica, linha, ultimos_jogos):
-    df_recente  = df.head(ultimos_jogos)
+
+    df_recente = df.head(ultimos_jogos)
 
     acertos = (df_recente[estatistica] >= linha).sum()
 
@@ -7,7 +8,23 @@ def tendencia_recente(df, estatistica, linha, ultimos_jogos):
 
     porcentagem = (acertos / total) * 100
 
+
+    print("\n📊 Últimos jogos analisados:\n")
+
+
+    for i, row in df_recente.iterrows():
+
+        print(
+            f"{row['DATA']} | vs {row['ADVERSARIO']} → "
+            f"{row[estatistica]} {estatistica}"
+        )
+
+
+    print("\nResumo:")
+
+
     print(
         f"{acertos}/{total} jogos ({porcentagem:.1f}%) "
-        f"com {linha}+ em {estatistica} nos últimos {ultimos_jogos} jogos"
+        f"com {linha}+ em {estatistica} "
+        f"nos últimos {ultimos_jogos} jogos"
     )
